e9161e518a4749a01386d3d496ce1536611c6b16
DaniyalAkhtar <daniyal.muet@gmail.com>
2025-02-01 15:29:13 +0100

    added function url

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterdaniyal.azurewebsites.net/api/GetResumeCounter?code=d5LSnl4IEqS1-3HqA_l6GQYepqwIbX_2m8yceuc8Iab8AzFuLpf7Aw%3D%3D';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response =>{
+    fetch(functionApiUrl).then(response =>{
         return response.json()
     }).then(response => {
         console.log("Website called function API");
